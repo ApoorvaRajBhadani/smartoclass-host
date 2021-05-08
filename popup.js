aeb28d68f458debb2ed1db9ff8cfae1594bccba8
Apoorva Raj Bhadani <apoorva222g@gmail.com>
2021-05-08 17:06:45 +0530

    Sign in working

--- popup.js
+++ popup.js
@@ -1,11 +1,12 @@
 $(function(){
+    //Initialize Popup.html
     chrome.storage.sync.get(['user','email'],function(userinfo){
         if(userinfo.user && userinfo.user != "none"){
             //user is signed in
             $("#btnSignIn").css('display','none');
             $("#email").css('display','none');
             $("#pass").css('display','none');
-            $("#userinfo").text(userinfo.email);
+            $("#userinfo").text("Logged in as "+userinfo.email);
         }else{
             //user is not signed in
             $("#btnSignOut").css('display','none');
@@ -13,44 +14,91 @@ $(function(){
             $("#userinfo").css('display','none');
         }
     });
+
+    //Sign in click listener
     $("#btnSignIn").click(function(){
+        //console.log(token);
         var email = $("#email").val();
         var pass = $("#pass").val();
         $("#email").val("");
         $("#pass").val("");
-        fetch('https://jsonplaceholder.typicode.com/posts', {
+        fetch('http://192.168.137.67:8000/api/login', {
             method: 'POST',
             body: JSON.stringify({
-                title: 'foo',
-                body: 'bar',
-                userId: 1,
+                email: email,
+                password: pass,
             }),
             headers: {
-                'Content-type': 'application/json; charset=UTF-8',
+                'Content-type': 'application/json',
             },
         })
-        .then((response) => response.json())
+        .then(function(response){
+            console.log(response.status);
+            return response.json();
+        })
         .then(function(json){
             //console.log(json.id);
-            const uid = json.id;
-            chrome.storage.sync.set({'user':uid,'email':email},function(){
+            if(json.id){
+                console.log(json);
+                console.log("login successful");
+                const uid = json.id;
+                chrome.storage.sync.set({'user':uid,'email':email,'token': json.token},function(){
 
-                $("#btnSignOut").css('display','block');
-                $("#btnTakeAttendance").css('display','block');
-                $("#userinfo").css('display','block');
-                $("#userinfo").text("Logged in as "+email);
-    
-                $("#btnSignIn").css('display','none');
-                $("#email").css('display','none');
-                $("#pass").css('display','none');
-            });
+                    $("#btnSignOut").css('display','block');
+                    $("#btnTakeAttendance").css('display','block');
+                    $("#userinfo").css('display','block');
+                    $("#userinfo").text("Logged in as "+email);
+        
+                    $("#btnSignIn").css('display','none');
+                    $("#email").css('display','none');
+                    $("#pass").css('display','none');
+                });
+            }else{
+                console.log(json);
+                console.log("login failed");
+                // close();
+            }
         });
     });
+
+    //Take Attendance click listener
     $("#btnTakeAttendance").click(function(){
-        
+        chrome.storage.sync.get(['user','token','meetId'],function(res){
+            console.log(res);
+                    // fetch('http://192.168.137.67:8000/api/login', {
+                    //     method: 'POST',
+                    //     body: JSON.stringify({
+                    //         email: email,
+                    //         password: pass,
+                    //     }),
+                    //     headers: {
+                    //         'Content-type': 'application/json',
+                    //     },
+                    // })
+                    fetch('https://jsonplaceholder.typicode.com/posts', {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            title: 'foo',
+                            body: 'bar',
+                            userId: 1,
+                        }),
+                        headers: {
+                            'Content-type': 'application/json; charset=UTF-8',
+                        },
+                    })
+                    .then(function(response){
+                        console.log(response.status);
+                        return response.json();
+                    })
+                    .then(function(json){
+                        console.log(json);
+                    });
+        });
     });
+
+    //Sign Out click listener
     $("#btnSignOut").click(function(){
-        chrome.storage.sync.set({'user':"none"},function(){
+        chrome.storage.sync.set({'user':"none",'token':"none"},function(){
 
             $("#btnSignOut").css('display','none');
             $("#btnTakeAttendance").css('display','none');
